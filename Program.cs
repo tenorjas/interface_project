8ae13ea20ed854ed9d84de16aea4540ad42b420b
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-08-21 15:59:29 -0400

    tested classes and interfaces, they're working

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace interface_project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var marine = new Marines();
+            marine.name = "Leroy Jethro Gibbs";
+            marine.rank = "Lieutenant";
+            marine.serialNumber = 871020079;
+
+            Console.WriteLine($"I am {marine.rank} {marine.name}, Serial Number {marine.serialNumber}");
+
+            Console.WriteLine(marine.Speak());
+            Console.WriteLine(marine.Eat());
+            Console.WriteLine(marine.Walk());
+
+            Console.WriteLine(marine.Load());
+            Console.WriteLine(marine.Aim());
+            Console.WriteLine(marine.PullTrigger());
+
+            Console.WriteLine(marine.Sleep());
         }
     }
 }
